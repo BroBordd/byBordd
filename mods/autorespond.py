4bb57eb410e07b28a3948dfd5b29622a6b032f7b
BroBordd <BroBordd@github.com>
2025-07-17 19:50:04 +0300

    ok

--- mods/autorespond.py
+++ mods/autorespond.py
@@ -1,4 +1,5 @@
-# Copyright 2025 - Solely by BrotherBoard - Feel free to utilize/modify this for personal use
+# Copyright 2025 - Solely by BrotherBoard
+# Intended for personal use only
 # Bug? Feedback? Telegram >> @GalaxyA14user
 
 """
