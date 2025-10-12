bd807ddaa52c1f8fa0d407ef6765ffb76c2a8b10
BroBordd <BroBordd@github.com>
2025-10-13 02:45:30 +0300

    FontMan: v1.0

---
+++ mods/dum.py
@@ -0,0 +1,15 @@
+# Copyright 2025 - Solely by BrotherBoard
+# Intended for personal use only
+# Bug? Feedback? Telegram >> @GalaxyA14user
+
+"""
+Dum v1.0 - Dum
+
+Dum
+"""
+
+from babase import Plugin
+
+# ba_meta require api 9
+# ba_meta export babase.Plugin
+class byBordd(Plugin): pass
