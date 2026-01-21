b9eb894cc589230e3912de9efde55c3544d4e9c4
BroBordd <BroBordd@github.com>
2026-01-22 01:45:03 +0200

    idk

--- mods/board.py
+++ mods/board.py
@@ -1829,7 +1829,7 @@ class Art:
         s.kids = [
             Widget.TEXT(
                 parent,
-                flatness=-2,
+                flatness=-2.5,
                 big=True,
                 text=t,
                 position=(letter_start_x + gap * i, letter_y),
