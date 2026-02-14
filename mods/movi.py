fa3f628140b7ec8a99d9fa0bdecbb1d507da1d6e
BroBordd <BroBordd@github.com>
2026-02-14 20:13:35 +0200

    idk

--- mods/movi.py
+++ mods/movi.py
@@ -3121,9 +3121,10 @@ class Editor:
         # wrap
         s.wrap([1,2,3],on_finish=lambda:(
             s.bottom_left(),
-            bui.apptimer(Const.BA_LAG_BIG,s.wrap),
-            print('yippe'),
-            bui.apptimer(5,s.wrap),
+            s.wrap_all()
+#            bui.apptimer(Const.BA_LAG_BIG,s.wrap),
+            print('wohooo'),
+#            bui.apptimer(5,s.wrap),
         ))
         # appear
         def appear():
